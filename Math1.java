22606b1ca90722afa4555bac4719abe4d50f59ab
pipipupucheeeck <genadijpetrov372@gmail.com>
2025-08-19 10:31:09 -0700

    Add files via upload

---
+++ Math1.java
@@ -0,0 +1,80 @@
+import javax.management.loading.PrivateClassLoader;
+
+class Main {
+  public static void main(String[] args) {
+
+    //Use the // to create single line comments to either add info or to take out code for debugging
+    //We will be using System.out.println and System.out.print throughout the year. Try using both below to see what the difference is!
+
+
+
+
+
+
+    //ANSWER: 
+
+
+
+    //Throughout the year this year we will need to store information. For that we will be using VARIABLES!
+
+    //Java is an Object-Oriented programming language. All variables we use this year will either be OBJECTS or PRIMITIVES
+
+    //There are 8 primitives in Java: int, byte, short, long, float, double, boolean, char
+
+    //For AP we need to know: int, double, boolean
+
+    //List examples of the types below and give definition
+    //int (integer): 
+    //double:
+    //boolean:
+
+    //For now we are just going to work with primitive
+
+    //Create 3 variables of each of the above types (USE GOOD CODING PRACTICE WHEN CREATING THE VARIABLES
+    int pcA = 1;
+    int pcB = 2;
+    int pcC = 3;
+
+    double price = 20.9;
+    double pi = 3.14;
+    double weather = 81.2;
+
+    boolean Goodweather = weather >= 81;
+    boolean Bigprice = price < 22;
+    
+    System.out.print("I want to buy it for ");
+
+    System.out.print(price);
+
+
+    //MATH TIME!
+
+    //What are the math operators that we can use?
+    //+ - * / %
+
+    //Try doing some math operations with numbers. How can we check to see if the math worked?
+    int sum = pcA + pcB;
+    System.out.println(sum);
+    System.out.println(5+7);
+    //Create codes 
+   
+    
+    //Odd integers from 1 to 100, inclusive of both
+
+
+
+    //All multiples of 3 from 1 to 100
+
+
+
+
+    //Starting at 1000, print on the same line (with a - [hypthen] between each) all of the numbers that end in 0 going down to 0
+  }
+}
+// class Main {
+//   public static void main(String[] args) {
+
+//     System.out.println("Hello world!");
+
+//   }
+// }
